022629277ac9a89706a94f5ce1189785cbd5ac90
Koc-Ali <1koc.ali@gmail.com>
2023-12-16 14:33:57 +0100

    release A5 part 1

--- task_5/2023_advent_coding_A5_1.py
+++ task_5/2023_advent_coding_A5_1.py
@@ -5,8 +5,8 @@ import re
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 
 # For accessing the file in the same folder
-# filename = "2023_advent_input_A5_1.txt"
-filename = "2023_advent_input_A5_1.sample.txt"
+filename = "2023_advent_input_A5_1.txt"
+# filename = "2023_advent_input_A5_1.sample.txt"
 
 # filename = "2023_advent_input_A5_2.txt"
 # filename = "2023_advent_input_A5_2.sample.txt"
@@ -16,7 +16,9 @@ path_to_file = fileDir + '/' + filename
 file = open(os.path.expanduser(path_to_file))
 
 # problem solution
+
 lowest_location = 0
+# mapping tables
 seeds = []
 seed_to_soil_map = []
 soil_to_fertilizer_map = []
@@ -26,59 +28,130 @@ light_to_temperature_map = []
 temperature_to_humidity_map = []
 humidity_to_location_map = []
 
-def read_seeds():
+mapped_soils = []
+mapped_fertilizers = []
+mapped_waters = []
+mapped_lights = []
+mapped_temperatures = []
+mapped_humidities = []
+mapped_locations = []
 
-def extract_seed_to_soil_map():
-def extract_soil_to_fertilizer_map():
-def extract_fertilizer_to_water_map():
-def extract_water_to_light_map():
-def extract_light_to_temperature_map():
-def extract_temperature_to_humidity_map():
-def extract_humidity_to_location_map():
+def extract_seeds(line):
+    seeds_numbers = re.findall('\d+', line)
+    # print('seed number = ', seeds_numbers)
+    return (seeds_numbers)
 
-def extract_numbers(card_line):
-    result_numbers = []
-    pattern = '\d+'
 
-    # find and delete pre-amble
-    pos_card_info = card_line.find(':')
-    pos_card_splitter = card_line.find('|')
-    winning_string = card_line[pos_card_info:pos_card_splitter]
-    own_string = card_line[pos_card_splitter:]
+def extract_mapping_lines(lines):
+    mapping_lines = []
+    for index, line in enumerate(lines):
+        mapping_numbers = re.findall('\d+', line)
+        # print('mapping umbers = ', mapping_numbers)
+        mapping_lines.append(mapping_numbers)
+    return (mapping_lines)
 
-    winning_numbers = re.findall(pattern, winning_string)
-    own_numbers = re.findall(pattern, own_string)
 
-    return winning_numbers, own_numbers
+def is_source_in_mapping(source, mapping):
+    if (int(source) >= int(mapping[1])) and (int(source) < int(mapping[1]) + int(mapping[2])):
+        return True
+    else:
+        return False
 
+
+def calc_destination(source, mapping_list):
+    # destination is source, if no mapping conversion is required
+    destination = int(source)
+
+    # perform mapping conversion
+    for mapping in mapping_list:
+        if is_source_in_mapping(source, mapping):
+            destination = int(source) - int(mapping[1]) + int(mapping[0])
+    return destination
+
+def calc_destination_list(source_list, mapping_list):
+    mapped_list = []
+    for source in source_list:
+        mapped_value = calc_destination(source, mapping_list)
+        mapped_list.append(mapped_value)
+    return (mapped_list)
+
+lines = []
+
+# read lines exclude empthy ones
 for line in file:
-    # read input file
-    almanac_line = line.strip()
+    if len(line.split()) == 0:
+        continue
+    else:
+        lines.append(line)
 
-    print(almanac_line)
-    # check section
-    if line ==
+for index, line in enumerate(lines):
+    if 'seeds' in line:
+        seeds_start = index
+    if 'seed-to-soil' in line:
+        seed_to_soil_map_start = index
+    if 'soil-to-fertilizer' in line:
+        soil_to_fertilizer_map_start = index
+    if 'fertilizer-to-water' in line:
+        fertilizer_to_water_map_start = index
+    if 'water-to-light' in line:
+        water_to_light_map_start = index
+    if 'light-to-temperature' in line:
+        light_to_temperature_map_start = index
+    if 'temperature-to-humidity' in line:
+        temperature_to_humidity_map_start = index
+    if 'humidity-to-location' in line:
+        humidity_to_location_map_start = index
 
+seeds = extract_seeds(lines[seeds_start])
+print(seeds)
 
-    winning_numbers, own_numbers = extract_numbers(card_line)
-    print(winning_numbers)
-    print(own_numbers)
+seed_to_soil_map = extract_mapping_lines(lines[seed_to_soil_map_start + 1:soil_to_fertilizer_map_start])
+print(seed_to_soil_map)
 
-    # calc points for all card
-    card_matches = 0
-    card_match_points = 0
-    for index, winning_numb in enumerate(winning_numbers):
-        # calc points for EACH card
-        if winning_numb in own_numbers:
-            card_matches += 1
-            if card_matches == 1:
-                card_match_points = 1
-            else:
-                card_match_points *= 2
-    total_worth_points += card_match_points
-    print('card match counted = ', card_matches)
-    print('card match points = ', card_match_points)
-    print('card total match points = ', total_worth_points)
+soil_to_fertilizer_map = extract_mapping_lines(lines[soil_to_fertilizer_map_start + 1: fertilizer_to_water_map_start])
+print(soil_to_fertilizer_map)
+
+fertilizer_to_water_map = extract_mapping_lines(lines[fertilizer_to_water_map_start + 1: water_to_light_map_start])
+print(fertilizer_to_water_map)
+
+water_to_light_map = extract_mapping_lines(lines[water_to_light_map_start + 1: light_to_temperature_map_start])
+print(water_to_light_map)
+
+light_to_temperature_map = extract_mapping_lines(
+    lines[light_to_temperature_map_start + 1: temperature_to_humidity_map_start])
+print(light_to_temperature_map)
+
+temperature_to_humidity_map = extract_mapping_lines(
+    lines[temperature_to_humidity_map_start + 1: humidity_to_location_map_start])
+print(temperature_to_humidity_map)
+
+humidity_to_location_map = extract_mapping_lines(lines[humidity_to_location_map_start + 1: len(lines)])
+print(humidity_to_location_map)
+
+# mapping
+
+mapped_soils = calc_destination_list(seeds, seed_to_soil_map)
+print('mapped souls: ', mapped_soils)
+
+mapped_fertilizers = calc_destination_list(mapped_soils, soil_to_fertilizer_map)
+print('mapped fertilizers: ', mapped_fertilizers)
+
+mapped_waters = calc_destination_list(mapped_fertilizers, fertilizer_to_water_map)
+print('mapped waters: ', mapped_waters)
+
+mapped_lights = calc_destination_list(mapped_waters, water_to_light_map)
+print('mapped lights: ', mapped_lights)
+
+mapped_temperatures = calc_destination_list(mapped_lights, light_to_temperature_map)
+print('mapped temperatures: ', mapped_temperatures)
+
+mapped_humidities = calc_destination_list(mapped_temperatures, temperature_to_humidity_map)
+print('mapped humudities: ', mapped_humidities)
+
+mapped_locations = calc_destination_list(mapped_humidities, humidity_to_location_map)
+print('mapped locations: ', mapped_locations)
+
+lowest_location = min(mapped_locations)
 
 # print final results
-print(' -- final result = ', total_worth_points)
+print(' -- final result = ', lowest_location)
--- task_5/2023_advent_coding_A5_2.py
+++ task_5/2023_advent_coding_A5_2.py
@@ -5,99 +5,153 @@ import re
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 
 # For accessing the file in the same folder
-# filename = "2023_advent_input_A4_1.txt"
-# filename = "2023_advent_input_A4_1.sample.txt"
+filename = "2023_advent_input_A5_1.txt"
+# filename = "2023_advent_input_A5_1.sample.txt"
 
-filename = "2023_advent_input_A4_2.txt"
-# filename = "2023_advent_input_A4_2.sample.txt"
+# filename = "2023_advent_input_A5_2.txt"
+# filename = "2023_advent_input_A5_2.sample.txt"
 
 path_to_file = fileDir + '/' + filename
 
 file = open(os.path.expanduser(path_to_file))
 
 # problem solution
-total_scratchcards = 0
-cards = []
-card_winning_numbers = []
-card_own_numbers = []
-total_worth_points = 0
-max_read_cards = 0
 
-def extract_numbers(card_line):
-    result_numbers = []
-    pattern = '\d+'
+lowest_location = 0
+# mapping tables
+seeds = []
+seed_to_soil_map = []
+soil_to_fertilizer_map = []
+fertilizer_to_water_map = []
+water_to_light_map = []
+light_to_temperature_map = []
+temperature_to_humidity_map = []
+humidity_to_location_map = []
 
-    # find and delete pre-amble
-    pos_card_info = card_line.find(':')
-    pos_card_splitter = card_line.find('|')
-    winning_string = card_line[pos_card_info:pos_card_splitter]
-    own_string = card_line[pos_card_splitter:]
-    card_number_string = card_line[:pos_card_info]
-    card_numbers = re.findall(pattern, card_number_string)
-    card_number = int(card_numbers[0])
-    winning_numbers = re.findall(pattern, winning_string)
-    own_numbers = re.findall(pattern, own_string)
+mapped_soils = []
+mapped_fertilizers = []
+mapped_waters = []
+mapped_lights = []
+mapped_temperatures = []
+mapped_humidities = []
+mapped_locations = []
 
-    return card_number, winning_numbers, own_numbers
+def extract_seeds(line):
+    seeds_numbers = re.findall('\d+', line)
+    # print('seed number = ', seeds_numbers)
+    return (seeds_numbers)
 
-def calc_matching_point(win_numbers, own_numbers):
-    # calc points for all card
-    card_matches = 0
-    card_match_points = 0
-    for index, winning_numb in enumerate(win_numbers):
-        # calc points for EACH card
-        if winning_numb in own_numbers:
-            card_matches += 1
-            if card_matches == 1:
-                card_match_points = 1
-            else:
-                card_match_points *= 2
-    return card_matches, card_match_points
 
-def extend_cards_according_match_points(match_card):
-    # print('match_card = ', match_card)
-    required_copies = match_card[1]
-    # print(' required copies = ', required_copies)
-    # check whole cards
-    for ccard in cards:
-        # if it matches to match_card, add required cards
-        if ccard[0] == match_card[0]:
-            for index in range(required_copies):
-                # get card to be copied
-                card_to_be_copied = cards[int(match_card[0])+index]
-                # print(' card to be copied = ', card_to_be_copied)
-                cards.append(card_to_be_copied)
-                # print(' new cards list = ', cards)
+def extract_mapping_lines(lines):
+    mapping_lines = []
+    for index, line in enumerate(lines):
+        mapping_numbers = re.findall('\d+', line)
+        # print('mapping umbers = ', mapping_numbers)
+        mapping_lines.append(mapping_numbers)
+    return (mapping_lines)
 
+
+def is_source_in_mapping(source, mapping):
+    if (int(source) >= int(mapping[1])) and (int(source) < int(mapping[1]) + int(mapping[2])):
+        return True
+    else:
+        return False
+
+
+def calc_destination(source, mapping_list):
+    # destination is source, if no mapping conversion is required
+    destination = int(source)
+
+    # perform mapping conversion
+    for mapping in mapping_list:
+        if is_source_in_mapping(source, mapping):
+            destination = int(source) - int(mapping[1]) + int(mapping[0])
+    return destination
+
+def calc_destination_list(source_list, mapping_list):
+    mapped_list = []
+    for source in source_list:
+        mapped_value = calc_destination(source, mapping_list)
+        mapped_list.append(mapped_value)
+    return (mapped_list)
+
+lines = []
+
+# read lines exclude empthy ones
 for line in file:
-    # read all cards extract numbers
-    card_line = line.strip()
-    max_read_cards += 1
+    if len(line.split()) == 0:
+        continue
+    else:
+        lines.append(line)
 
-    # print(card_line)
-    card_number, winning_numbers, own_numbers = extract_numbers(card_line)
-    # print(winning_numbers)
-    # print(own_numbers)
+for index, line in enumerate(lines):
+    if 'seeds' in line:
+        seeds_start = index
+    if 'seed-to-soil' in line:
+        seed_to_soil_map_start = index
+    if 'soil-to-fertilizer' in line:
+        soil_to_fertilizer_map_start = index
+    if 'fertilizer-to-water' in line:
+        fertilizer_to_water_map_start = index
+    if 'water-to-light' in line:
+        water_to_light_map_start = index
+    if 'light-to-temperature' in line:
+        light_to_temperature_map_start = index
+    if 'temperature-to-humidity' in line:
+        temperature_to_humidity_map_start = index
+    if 'humidity-to-location' in line:
+        humidity_to_location_map_start = index
 
-    card_matches, card_match_points = calc_matching_point(winning_numbers, own_numbers)
+seeds = extract_seeds(lines[seeds_start])
+print(seeds)
 
-    total_worth_points += card_match_points
+seed_to_soil_map = extract_mapping_lines(lines[seed_to_soil_map_start + 1:soil_to_fertilizer_map_start])
+print(seed_to_soil_map)
 
-    cards.append([card_number, card_matches])
+soil_to_fertilizer_map = extract_mapping_lines(lines[soil_to_fertilizer_map_start + 1: fertilizer_to_water_map_start])
+print(soil_to_fertilizer_map)
 
-    # print('card match counted = ', card_matches)
-    # print('card match points = ', card_match_points)
-    # print('card total match points = ', total_worth_points)
-    # print('card match points = ', cards)
+fertilizer_to_water_map = extract_mapping_lines(lines[fertilizer_to_water_map_start + 1: water_to_light_map_start])
+print(fertilizer_to_water_map)
 
+water_to_light_map = extract_mapping_lines(lines[water_to_light_map_start + 1: light_to_temperature_map_start])
+print(water_to_light_map)
 
-# iterate all orginal card sets and add cards according to cards match points
-for card in cards[:max_read_cards]:
-    extend_cards_according_match_points(card)
-    #print(card)
-    #print(cards)
+light_to_temperature_map = extract_mapping_lines(
+    lines[light_to_temperature_map_start + 1: temperature_to_humidity_map_start])
+print(light_to_temperature_map)
+
+temperature_to_humidity_map = extract_mapping_lines(
+    lines[temperature_to_humidity_map_start + 1: humidity_to_location_map_start])
+print(temperature_to_humidity_map)
+
+humidity_to_location_map = extract_mapping_lines(lines[humidity_to_location_map_start + 1: len(lines)])
+print(humidity_to_location_map)
+
+# mapping
+
+mapped_soils = calc_destination_list(seeds, seed_to_soil_map)
+print('mapped souls: ', mapped_soils)
+
+mapped_fertilizers = calc_destination_list(mapped_soils, soil_to_fertilizer_map)
+print('mapped fertilizers: ', mapped_fertilizers)
+
+mapped_waters = calc_destination_list(mapped_fertilizers, fertilizer_to_water_map)
+print('mapped waters: ', mapped_waters)
+
+mapped_lights = calc_destination_list(mapped_waters, water_to_light_map)
+print('mapped lights: ', mapped_lights)
+
+mapped_temperatures = calc_destination_list(mapped_lights, light_to_temperature_map)
+print('mapped temperatures: ', mapped_temperatures)
+
+mapped_humidities = calc_destination_list(mapped_temperatures, temperature_to_humidity_map)
+print('mapped humudities: ', mapped_humidities)
+
+mapped_locations = calc_destination_list(mapped_humidities, humidity_to_location_map)
+print('mapped locations: ', mapped_locations)
+
+lowest_location = min(mapped_locations)
 
 # print final results
-# print(' -- final result cards = ', cards)
-print(' -- final result cards amount = ', len(cards))
-
+print(' -- final result = ', lowest_location)
